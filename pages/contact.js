048a90fbbbaf2b7309545e20ae07a2f695a83768
Andriy Koval <akovalwm@gmail.com>
2022-11-08 11:19:04 -0500

    updated email

--- pages/contact.js
+++ pages/contact.js
@@ -18,7 +18,7 @@ export default function contact() {
         <Header></Header>
         <div className='contactme'>
           <h1>Get in touch</h1>
-          <p>The fastest way to contact me is via <a className='blue-underline' href="https://www.linkedin.com/in/kovalandriy/" target="_blank">Linkedin</a> or by simply sending an email to <button className='emailButton blue-underline' type="submit" onClick={() => setButtonText("akovalwm@gmail.com")} > {buttonText} </button></p>
+          <p>The fastest way to contact me is via <a className='blue-underline' href="https://www.linkedin.com/in/kovalandriy/" target="_blank">Linkedin</a> or by simply sending an email to <button className='emailButton blue-underline' type="submit" onClick={() => setButtonText("akovaldev@gmail.com")} > {buttonText} </button></p>
         </div>
         
         
