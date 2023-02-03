d6de5f19c65948d1d7c2945f22db523ec7850945
Andriy Koval <akovalwm@gmail.com>
2023-02-03 13:01:30 -0500

    latest changes

--- pages/index.js
+++ pages/index.js
@@ -30,10 +30,10 @@ export default function Home({users}) {
         <h1>Hello</h1>
 
         <p >
-          My name is Andriy Koval, and I am a front end web developer. Currently, I am building great web experiences at <a className="rubine-text" target="_blank" href="https://www.uberflip.com" rel="noindex nofollow">Uberflip</a>. 
+          My name is Andriy Koval, and I am a front end web developer with over 5 years of experience and a track record of delivering projects on time and within budget.
         </p>
         <p >
-            I enjoy challenges and finding creative solutions to solve problems thus I am facing programing challenges with excitement. I thrive on continuous learning and I love discovering new skills. When I am not coding I am usually out with my camera or spending time with friends and family or catching up some tv shows. <Link href="/about">
+            I enjoy challenges and finding creative solutions to solve problems thus I am facing programing challenges with excitement. I thrive on continuous learning and I love discovering new skills. I enjoy solving complex problems and delivering high-quality results. I am passionate about creating user-friendly and visually appealing websites and applications. When I am not coding I am usually out with my camera or spending time with friends and family or catching up some tv shows. <Link href="/about">
             <a className='blue-underline'>Continue reading...</a>
           </Link>
         </p>
